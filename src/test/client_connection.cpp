2c48e679af259f79ca4f91a73bbfb46706214d8b
Valentin Rusu <kde@rusu.info>
2014-12-28 12:58:44 +0100

    No longer autostart VPD when existing vpd process is found

--- src/test/client_connection.cpp
+++ src/test/client_connection.cpp
@@ -21,56 +21,117 @@
 #define BOOST_TEST_MODULE ClientConnection
 #include "client_connection.h"
 
+#include <sys/types.h>
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <thread>
 #include <future>
-#include <sys/types.h>
-#include <signal.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/asio.hpp>
 #include <boost/process.hpp>
 
 namespace io = boost::asio;
 namespace bp = boost::process;
+using namespace std;
+
+int getProcIdByName(string procName)
+{
+    int pid = -1;
+
+    // Open the /proc directory
+    DIR *dp = opendir("/proc");
+    if (dp != NULL)
+    {
+        // Enumerate all entries in directory until process found
+        struct dirent *dirp;
+        while (pid < 0 && (dirp = readdir(dp)))
+        {
+            // Skip non-numeric entries
+            int id = atoi(dirp->d_name);
+            if (id > 0)
+            {
+                // Read contents of virtual /proc/{pid}/cmdline file
+                string cmdPath = string("/proc/") + dirp->d_name + "/cmdline";
+                ifstream cmdFile(cmdPath.c_str());
+                string cmdLine;
+                getline(cmdFile, cmdLine);
+                if (!cmdLine.empty())
+                {
+                    // Keep first cmdline item which contains the program path
+                    size_t pos = cmdLine.find('\0');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(0, pos);
+                    // Keep program name only, removing the path
+                    pos = cmdLine.rfind('/');
+                    if (pos != string::npos)
+                        cmdLine = cmdLine.substr(pos + 1);
+                    // Compare against requested process name
+                    if (procName == cmdLine)
+                        pid = id;
+                }
+            }
+        }
+    }
+
+    closedir(dp);
+
+    return pid;
+}
 
 struct AsioSetup {
     AsioSetup() {
         BOOST_TEST_MESSAGE("setup test fixture");
         io::ip::tcp::resolver resolver(io_service_);
         epi_ = resolver.resolve({"127.0.0.1", "7700"});
-        
-        std::thread vpd = std::thread(
-                [this]() {
-                    BOOST_TEST_MESSAGE("launching VPD...");
-                    auto execPath = "../vpd"; 
-                    // auto execArgs = {
-                    // };
-                    //vlc.ctx_.process_name = "cvlc";
+       
+        int existing_vpd_id = getProcIdByName("vpd");
+        if (-1 == existing_vpd_id) {
+            std::thread vpd = std::thread(
+                    [this]() {
+                        BOOST_TEST_MESSAGE("launching VPD...");
+                        auto execPath = "../vpd"; 
+                        // auto execArgs = {
+                        // };
+                        //vlc.ctx_.process_name = "cvlc";
 
-                    bp::child vpd = bp::create_child(execPath);
-                    vpd_pid_ = vpd.get_id();
-                    bp::status vpdstatus(io_service_);
-                    vpdstatus.async_wait(vpd.get_id(), 
-                        [](const boost::system::error_code &ec, int exit_code){
-                           BOOST_TEST_MESSAGE("VPD exit_code:" << exit_code); 
-                        });
-                    BOOST_TEST_MESSAGE("VPD thread exit");
-                });
-        asio_thread_ = std::thread(
-                [this]() {
-                    io_service_.run();
-                });
-        // wait for the vpd process start
-        // FIXME we should wait more reliably for VPD start, as maybe in the future
-        // it'll take longer to initialize
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        vpd.detach();
+                        bp::child vpd = bp::create_child(execPath);
+                        vpd_pid_ = vpd.get_id();
+                        bp::status vpdstatus(io_service_);
+                        vpdstatus.async_wait(vpd.get_id(), 
+                            [](const boost::system::error_code &ec, int exit_code){
+                               BOOST_TEST_MESSAGE("VPD exit_code:" << exit_code); 
+                            });
+                        BOOST_TEST_MESSAGE("VPD thread exit");
+                    });
+            // wait for the vpd process start
+            // FIXME we should wait more reliably for VPD start, as maybe in the future
+            // it'll take longer to initialize
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            vpd.detach();
+        } else {
+            BOOST_TEST_MESSAGE("using existing VPD pid: " << existing_vpd_id);
+        }
+        // asio_thread_ = std::thread(
+        //         [this]() {
+        //             BOOST_TEST_MESSAGE("Starting asio thread");
+        //             io::io_service::work work(io_service_); 
+        //             BOOST_TEST_MESSAGE("asio thread stop");
+        //         });
     }
     ~AsioSetup() {
         BOOST_TEST_MESSAGE("teardown test fixture");
         io_service_.stop();
         // FIXME figure out how to terminate the child using bp::process:terminate
-        kill(vpd_pid_, SIGKILL);
+        if (vpd_pid_ >0)
+            kill(vpd_pid_, SIGKILL);
     }
 
     io::io_service io_service_;
@@ -88,6 +149,7 @@ BOOST_AUTO_TEST_CASE( server_accept_connection ) {
     socket.connect(*epi_, ec);
     BOOST_MESSAGE("socket connect error_code: " << ec.value() << ", message: " << ec.message());
     BOOST_CHECK(!ec);
+    socket.close();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
